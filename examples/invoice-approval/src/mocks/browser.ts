560e028eeb18f6bf4c167dd17ab034722aff1f6e
Rustam Giliaziev <gilaziev@gmail.com>
2026-07-27 22:39:29 +0400

    feat: compose invoice example bootstrap

--- examples/invoice-approval/src/mocks/browser.ts
+++ examples/invoice-approval/src/mocks/browser.ts
@@ -2,11 +2,24 @@ import { setupWorker } from "msw/browser"
 import { createMockInvoiceStore } from "./data"
 import { createInvoiceHandlers } from "./handlers"
 
-const store = createMockInvoiceStore()
+export async function startInvoiceMockWorker(
+	baseUrl: URL,
+): Promise<() => void> {
+	const worker = setupWorker(
+		...createInvoiceHandlers({
+			baseUrl,
+			store: createMockInvoiceStore(),
+		}),
+	)
 
-export const worker = setupWorker(
-	...createInvoiceHandlers({
-		baseUrl: new URL("/api/", window.location.origin),
-		store,
-	}),
-)
+	await worker.start({
+		onUnhandledRequest: "error",
+		serviceWorker: {
+			url: "/mockServiceWorker.js",
+		},
+	})
+
+	return () => {
+		worker.stop()
+	}
+}
