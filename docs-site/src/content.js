f698846dabdb59d92cab44f1b7c36db8f2805413
Rustam Giliaziev <gilaziev@gmail.com>
2026-07-28 14:02:29 +0400

    docs: change label

--- docs-site/src/content.js
+++ docs-site/src/content.js
@@ -178,7 +178,7 @@ export const ui = {
 		openDemo: "Open live example",
 	},
 	ru: {
-		docs: "Документация",
+		docs: "Дока",
 		search: "Поиск по документации…",
 		searchResults: "Подходящие главы",
 		github: "GitHub",
